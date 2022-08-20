30a89b64fb4463dd4d527c238461674fba7cfb30
Gaspar Acevedo <acevedo.zain.gaspar@gmail.com>
2022-08-20 00:47:16 -0300

    [TP01] Arreglo solución del Ejercicio 8

--- tps/tp01/tp1_ej08.py
+++ tps/tp01/tp1_ej08.py
@@ -34,44 +34,30 @@ def inicializar(conn):
 # Funcion que dada una linea del archivo CSV (en forma de objeto) va a encargarse de insertar el (o los) objetos
 # necesarios
 def procesar_fila(database, fila):
-
-    tupla_a_insertar = (
-        fila["id_especialidad"],
-        fila["nombre_especialidad"],
-        fila["id_torneo"],
-        fila["nombre_torneo"],
-        fila["id_deportista"],
-        fila["nombre_deportista"],
-        int(fila["marca"]), # importante hacer el cast a int
-        fila["nombre_tipo_especialidad"]
-    )
-
-    # si la tupla no está en la database, la agregamos (append)
-    if tupla_a_insertar not in database:
-        database.append(tupla_a_insertar)
+    database.append(fila)
 
 
 # Funcion que realiza el o los queries que resuelven el ejercicio, utilizando la base de datos.
 def generar_reporte(database):
     archivo = codecs.open(nombre_archivo_resultado_ejercicio, encoding='utf-8', mode='w')
 
-    nombre_torneos = set(map(lambda x : x[3], database))
+    nombre_torneos = set(map(lambda x : x["nombre_torneo"], database))
     nombre_torneos_ordenados = sorted(nombre_torneos, key = lambda x : x)
     
-    agrupacion_por_nombre_torneo = [[registro for registro in database if registro[3] == nombre_torneo] for nombre_torneo in nombre_torneos_ordenados]
+    agrupacion_por_nombre_torneo = [[registro for registro in database if registro["nombre_torneo"] == nombre_torneo] for nombre_torneo in nombre_torneos_ordenados]
 
     for grupo_torneo in agrupacion_por_nombre_torneo:
-        nombres_especialidades = set(map(lambda x: x[1], grupo_torneo))
+        nombres_especialidades = set(map(lambda x: x["nombre_especialidad"], grupo_torneo))
         nombres_especialidades_ordenadas = sorted(nombres_especialidades, key = lambda x : x)
 
-        agrupacion_torneo_por_especialidad = [[registro for registro in grupo_torneo if registro[1] == nombre_especialidad] for nombre_especialidad in nombres_especialidades_ordenadas]
+        agrupacion_torneo_por_especialidad = [[registro for registro in grupo_torneo if registro["nombre_especialidad"] == nombre_especialidad] for nombre_especialidad in nombres_especialidades_ordenadas]
         for grupo_torneo_especialidad in agrupacion_torneo_por_especialidad:
-            nombre_tipo_especialidad = grupo_torneo_especialidad[0][7] ## el valor será el mismo en todos los registros de cada grupo, por eso con el primero basta
-            grupo_ordenado_por_marcas = sorted(grupo_torneo_especialidad, key = lambda x : x[6], reverse = ordenDescendiente(nombre_tipo_especialidad))
+            nombre_tipo_especialidad = grupo_torneo_especialidad[0]["nombre_tipo_especialidad"] ## el valor será el mismo en todos los registros de cada grupo, por eso con el primero basta
+            grupo_ordenado_por_marcas = sorted(grupo_torneo_especialidad, key = lambda x : x["marca"], reverse = ordenDescendiente(nombre_tipo_especialidad))
             # Procesamos el TOP 3
             for elemento in grupo_ordenado_por_marcas[: 3]:
                 elemento_a_insertar = ", ".join(
-                [str(elemento[i]) if elemento[i] is not None else "" for i in (3,1,5,6)])
+                [str(elemento[i]) if elemento[i] is not None else "" for i in ("nombre_torneo", "nombre_especialidad", "nombre_deportista", "marca")])
             
                 grabar_linea(archivo, elemento_a_insertar)
 
